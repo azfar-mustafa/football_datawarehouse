f5bd1512c973be792ee03d0aa59f6f6666195a05
azfar-mustafa <khairulbakar95@gmail.com>
2022-05-20 23:32:56 +0800

    Add command to create role assignment

--- azure_function_ingest/assign_identity.sh
+++ azure_function_ingest/assign_identity.sh
@@ -5,5 +5,3 @@
 
 # Assign identity to the app
 az webapp identity assign --resource-group $resourceGroup --name $appName --identities $resourceid
-
-az role assignment create --role "Storage Blob Data Reader" --assignee $principalid
